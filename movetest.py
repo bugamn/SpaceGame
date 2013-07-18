f913cc4ce5359600897496c1fa64c1684a890d84
Bugamn <bugamn@den>
2013-07-18 00:21:49 -0300

    Add "unofficial" vector library.
    Source:
    http://pygame.org/wiki/2DVectorClass
    
    Need to update, probably.

--- movetest.py
+++ movetest.py
@@ -1,47 +1,56 @@
 import pygame
+from pygame.sprite import Sprite
+from vec2d import vec2d
 
 size = height, width = 640, 480
 
-class Vector:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+class GameObject(Sprite):
+    def __init__(self, screen, img_filename, position, direction, speed):
+        Sprite.__init__(self)
+        self.screen = screen
 
-class GameObject:
-    def __init__(self, image, position, speed):
         self.speed = speed
-        self.image = image
-        self.pos = image.get_rect().move(position.x, position.y)
+        self.base_image = pygame.image.load(img_filename).convert_alpha()
+        self.image = self.base_image
+        self.pos = vec2d(position)
+        self.direction = vec2d(direction).normalized()
+        self.image_w, self.image_h = self.image.get_size()
+        self.bounds_rect = self.image.get_rect()
 
     def move(self):
-        self.pos = self.pos.move(self.speed.x, self.speed.y)
-        if self.pos.right > height or self.pos.left < 0:
+        self.pos += self.speed
+        if self.bounds_rect.right > height or self.bounds_rect.left < 0:
             self.speed.x = -self.speed.x
-        #if self.pos.top < 0 or self.pos.bottom > width:
-            #self.speed.y = -self.speed.y
+        if self.bounds_rect.top < 0 or self.bounds_rect.bottom > width:
+            self.speed.y = -self.speed.y
 
-# TODO considerar usar NumPy para vetores
+    def blitme(self):
+        draw_pos = self.image.get_rect().move(
+                self.pos.x - self.image_w / 2,
+                self.pos.y - self.image_h / 2)
+        self.screen.blit(self.image, draw_pos)
+
 def main():
     pygame.init()
-    
-    screen = pygame.display.set_mode(size)
-    player = pygame.image.load('images/disk.bmp').convert()
-    beam = pygame.image.load('images/beam.bmp').convert()
-    background = pygame.image.load('images/background.bmp').convert()
-    screen.blit(background, (0,0))
 
-    obj = GameObject(player, Vector(height/2, width/2), Vector(0, 0))
+    screen = pygame.display.set_mode(size)
+    player_img = 'images/disk.bmp'
+    beam_img = 'images/beam.bmp'
+    background = pygame.image.load('images/background.bmp').convert()
+
+    obj = GameObject(screen, player_img, (height/2, width/2), (1,0), (0, 0))
     shots = []
 
     while True:
+        screen.blit(background, (0,0))
         for event in pygame.event.get():
             if event.type in (pygame.QUIT,):
                 return
 
         keys = pygame.key.get_pressed()
-        speed = Vector(0,0)
+        speed = vec2d((0,0))
         if keys[pygame.K_a]:
-            shots.append(GameObject(beam, Vector(obj.pos.x, obj.pos.y), Vector(0, 20)))
+            shots.append(GameObject(screen, beam_img, (obj.pos.x, obj.pos.y), obj.direction, (0, 20)))
         if keys[pygame.K_UP]:
             speed.y = -5
         elif keys[pygame.K_DOWN]:
@@ -51,7 +60,7 @@ def main():
         elif keys[pygame.K_RIGHT]:
             speed.x = 5
         if keys[pygame.K_SPACE]:
-            shots.append(GameObject(beam, Vector(obj.pos.x, obj.pos.y), Vector(0, -20)))
+            shots.append(GameObject(screen, beam_img, (obj.pos.x, obj.pos.y), obj.direction, (0, -20)))
         if keys[pygame.K_q]:
             print len(shots)
             print obj.pos
@@ -59,17 +68,16 @@ def main():
             return
         obj.speed = speed
 
-        screen.blit(background, obj.pos, obj.pos)
         obj.move()
-        screen.blit(obj.image, obj.pos)
+        obj.blitme()
         for s in shots:
-            screen.blit(background, s.pos, s.pos)
+            s.blitme()
         for s in shots:
             s.move()
-            if s.pos.bottom < 0:
+            if s.bounds_rect.bottom < 0:
                 shots.remove(s)
             else:
-                screen.blit(s.image, s.pos)
+                s.blitme()
 
         pygame.display.update()
         pygame.time.delay(17)
